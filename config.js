5b93118fd1bfb26d09d5d4afe3f5c116a04844e5
CA <47811162+lightants@users.noreply.github.com>
2026-09-01 08:50:40 -0700

    Connect VA leads Google Sheet webhook

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Sheets webhook URL after Apps Script deploy.
-window.MSPACE_SHEETS_WEBHOOK = "";
+window.MSPACE_SHEETS_WEBHOOK = "https://script.google.com/macros/s/AKfycbzEopWgT59fRyAwp4w_HqKcgHLZ65khpKKkUTf0GfEoUMnZf_1hA0gzvaX3RyK8no3X/exec";
 window.MSPACE_FIREBASE = {
   apiKey: "AIzaSyBRwgJCKF0Ptw5jCF_sEroIznRP5g7axg0",
   authDomain: "mspace-resume-4e55a.firebaseapp.com",
